17e4c72bed59db567fdb7199bd9ad14cd22622e6
MaYi <mayi@wiredcraft.com>
2025-11-22 20:21:45 +0800

    clean the codebase and update the lock.yml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
-import Head from 'next/head';
 import KVSection from '../components/compound/KVSection';
 import FolderSection from '../components/compound/FolderSection';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      <Head>
-        <title>知乎 2025 年度盘点</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-      </Head>
-
       <main className="w-full max-w-[500px] mx-auto relative">
         {/* KV 部分 */}
         <KVSection />

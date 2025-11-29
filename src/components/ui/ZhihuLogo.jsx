46e2857577f7c177cc57cf9b1ca5d029a9bfbdad
jason wang <418022698@qq.com>
2025-11-29 16:20:38 +0800

    pc style

--- src/components/ui/ZhihuLogo.jsx
+++ src/components/ui/ZhihuLogo.jsx
@@ -5,22 +5,19 @@ import { useAssets } from '@/context/assets-context';
 
 const ZhihuLogo = ({ className = '' }) => {
   const { assets } = useAssets();
-  
+
   if (!assets) return null;
-  
+
   const logoAsset = assets.kv.logo;
-  // Display at 50% of original size for responsive design
-  const displayWidth = logoAsset.width * 0.5;
-  const displayHeight = logoAsset.height * 0.5;
-  
+
   return (
-    <div className={`relative z-50 ${className}`}>
-      <Image 
+    <div className={`relative z-50 ${className} px-[140px]`}>
+      <Image
         src={logoAsset.url}
         alt={logoAsset.alt}
-        width={logoAsset.width}
-        height={logoAsset.height}
-        style={{ width: `${displayWidth}px`, height: `${displayHeight}px` }}
+        width={logoAsset.width / 2}
+        height={logoAsset.height / 2}
+        style={{ width: '100%' }}
         className="object-contain"
       />
     </div>
